e32c3aea30666a9009f1b2977ff1003138bf3df7
SafetKaric <safetkaric247@gmail.com>
2022-10-06 00:51:23 +0200

    Basic

---
+++ index.php
@@ -0,0 +1,15 @@
+<!-- <!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo("charset"); ?>">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class("example"); ?>>
+    <?php wp_body_open(); ?>
+
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, iure.</p>
+
+    <?php wp_footer(); ?>
+
+</body>
+</html> -->
